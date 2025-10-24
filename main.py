9e4c67e0c6791a83ffa589e00fc9cc52842f8177
JCKawin <jckawin2007@gmail.com>
2025-10-24 17:13:40 +0530

    working

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 import pygame
+from constants import *
+import sys
 
 class Main():
     def __init__(self):
         pygame.init()
+        try : pygame.mixer.init()
+        except : print("NO Sound Device Detected")
+        try : 
+            pygame.display.init()
+            self.screen = pygame.display.set_mode(RES) 
+
+        except: print("NO Display found")
+
+
+
         self._load()
+        self.clock = pygame.time.Clock()
         self.running = True
+
     def _load(self):
         pass
 
     def run(self):
         while self.running:
             for event in pygame.event.get():
-                if event == pygame.QUIT:
-                    break
+                if event.type == pygame.QUIT:
+                    sys.exit()
+        
+            self.screen.fill(BG_COLOR)
+            pygame.display.flip()
 
         else:
             self._gameover()
