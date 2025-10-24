05e4bb1504d48a7367b11511112c36e42a18d165
JCKawin <jckawin2007@gmail.com>
2025-10-24 15:48:20 +0530

    some boiler plate added

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-def main():
-    print("Hello from yoi-yurei!")
+import pygame
 
+class Main():
+    def __init__(self):
+        pygame.init()
+        self._load()
+        self.running = True
+    def _load(self):
+        pass
+
+    def run(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event == pygame.QUIT:
+                    break
+
+        else:
+            self._gameover()
+    
+    def _gameover(self):
+        pass
+
+    def __del__(self):
+        pygame.quit()
 
 if __name__ == "__main__":
-    main()
+    Game : Main = Main()
+    Game.run()
