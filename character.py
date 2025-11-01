d945950d5f3f5f1eb0637eaed16a8ada13572692
Aadi-Vibhuti <aadivibhuti10@gmail.com>
2025-11-01 11:56:35 +0530

    grav

--- character.py
+++ character.py
@@ -6,8 +6,9 @@ class BaseCharacter(pygame.sprite.Sprite):
         self.image = pygame.Surface((100 , 100))
         self.rect = self.image.get_frect()
         self.rect.center = main.screen.get_rect().center
-        self.direction = pygame.Vector2(0,0)
-        self.velocity = 1
+        # self.direction = pygame.Vector2(0,0)
+        self.velocity = pygame.Vector2(1,0)
+        self.falling=False
         self.gravity = pygame.Vector2(0,-9.8)
 
     def draw(self , screen):
@@ -15,16 +16,20 @@ class BaseCharacter(pygame.sprite.Sprite):
 
 
     def update(self , key , dt):
-        self.direction.x = int(key[pygame.K_d]) - int(key[pygame.K_a])
-        self.rect.x += ( self.direction.x * self.velocity) * dt #type: ignore
-        if key[pygame.K_SPACE]:
-            self.rect.y -= 20 * dt
+        self.velocity.x = int(key[pygame.K_d]) - int(key[pygame.K_a])
+        key_get = pygame.key.get_just_pressed()
+        self.rect.center += self.velocity * dt
+        if self.falling:
+            self.velocity.y+=GRAVITY*dt
+        if key_get[pygame.K_SPACE]:
+            print("space pressed",dt)
+            self.velocity.y -= 20 * dt
 
 
-        if self.rect.bottom > RES[1] - 100:
-            self.rect.y -= 0
-        else:
-            self.rect.y += 0.9 * dt
+        # if self.rect.bottom > RES[1] - 100:
+        #     self.rect.y -= 0
+        # else:
+        #     self.rect.y += 0.9 * dt
             
 
         
--- levels.py
+++ levels.py
@@ -2,19 +2,31 @@ import pygame
 import character
 from constants import *
 
+class floor:
+    def __init__(self):
+        self.surface= pygame.surface.Surface((RES[0] , 100))
+        self.rect= self.surface.get_rect(bottom=RES[1])
+
 class level1:
     def __init__(self , main) -> None:
         self.screen : pygame.surface.Surface = main.screen
         self.character = character.BaseCharacter(self)
-        self.floor = pygame.surface.Surface((RES[0] , 100))
+        self.floor = floor()
 
     def run(self , dt):
         key = pygame.key.get_pressed()
         self.screen.fill("white")
         self.character.draw(self.screen)
-        self.screen.blit(self.floor , (0 , RES[1]-100))
-        self.character.update(key , dt)
+        self.screen.blit(self.floor.surface , self.floor.rect)
+        #gravity
+        if self.character.rect.bottom<self.floor.rect.top:
+            self.character.falling=True
+        else:
+            self.character.velocity.y=0
+            self.character.falling=False
 
+        self.character.update(key , dt)
         #collisions
+
 
     
